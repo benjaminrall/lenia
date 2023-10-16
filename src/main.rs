14d857c83a3682996aab6d13a751694e59a6c02a
benjaminrall <benr2508@aol.com>
2023-10-17 00:36:48 +0100

    Basic Pixel Canvas Test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use pixel_canvas::{Canvas, Color};
+use pixel_canvas::input::MouseState;
+
 fn main() {
-    println!("Hello, world!");
+    let canvas = Canvas::new(512, 512)
+        .title("Test")
+        .state(MouseState::new())
+        .input(MouseState::handle_input);
+
+    canvas.render(|mouse, image| {
+        let width = image.width();
+        for (y, row) in image.chunks_mut(width).enumerate() {
+            for (x, pixel) in row.iter_mut().enumerate() {
+                let dx = x as i32 - mouse.x;
+                let dy = y as i32 - mouse.y;
+                let dist = dx * dx + dy * dy;
+                *pixel = Color {
+                    r: if dist < 128 * 128 { dy as u8 } else { 0 },
+                    g: if dist < 128 * 128 { dx as u8 } else { 0 },
+                    b: 0
+                }
+            }
+        }
+    });
 }
